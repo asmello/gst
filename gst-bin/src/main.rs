5ea06be3363a209692405e57e364ca07ac84283e
André Sá de Mello <3285133+asmello@users.noreply.github.com>
2023-06-01 23:07:15 +0100

    implement the glcs algorithm

--- gst-bin/src/main.rs
+++ gst-bin/src/main.rs
@@ -1,16 +1,64 @@
+use clap::{Args, Parser, Subcommand};
 use gst::GeneralizedSuffixTree;
+use std::{
+    fs::File,
+    io::{stdin, BufRead, BufReader, Error},
+};
 
-use anyhow::{bail, Result};
+/// Algorithms based on the Generalized Suffix Tree
+#[derive(Parser)]
+#[command(author = "André Sá de Mello <asmello@pm.me>")]
+#[command(version = "0.1.0")]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
 
-fn main() -> Result<()> {
-    let inputs: Vec<String> = std::env::args().skip(1).collect();
-    if inputs.len() != 1 {
-        bail!("Must provide exactly 1 argument (filename)");
+#[derive(Subcommand)]
+enum Command {
+    #[command(name = "glcs")]
+    GeneralizedLongestCommonSubsequence(GeneralizedLongestCommonSubsequenceArgs),
+}
+
+#[derive(Args)]
+struct GeneralizedLongestCommonSubsequenceArgs {
+    /// File to read strings from (if not specified, reads from stdin by default)
+    #[arg(short, long)]
+    file: Option<String>,
+
+    #[arg(short, long, default_value_t = 2)]
+    threshold: usize,
+
+    #[arg(short, long, default_value_t = 2)]
+    min_length: usize,
+}
+
+fn run_glcp(args: GeneralizedLongestCommonSubsequenceArgs) -> Result<(), Error> {
+    let gst = if let Some(file) = args.file.as_deref() {
+        let lines = BufReader::new(File::open(file)?)
+            .lines()
+            .map(|res| res.expect("IO error").chars().collect::<Vec<char>>());
+        GeneralizedSuffixTree::from_iter(lines)
+    } else {
+        let lines = stdin()
+            .lock()
+            .lines()
+            .map(|res| res.expect("IO error").chars().collect::<Vec<char>>());
+        GeneralizedSuffixTree::from_iter(lines)
+    };
+
+    for common in gst.find_common(args.threshold, args.min_length) {
+        println!("{}", String::from_iter(common));
     }
-    for input in inputs {
-        let data: Vec<_> = std::fs::read_to_string(input)?.chars().collect();
-        let st = GeneralizedSuffixTree::from([data]);
-        println!("{st}");
-    }
+
     Ok(())
+}
+
+fn main() -> Result<(), Error> {
+    let args = Cli::parse();
+
+    match args.command {
+        Command::GeneralizedLongestCommonSubsequence(sub_args) => run_glcp(sub_args),
+    }
 }
--- gst/src/lib.rs
+++ gst/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::fmt::Debug;
 use std::hash::Hash;
 
@@ -26,7 +26,7 @@ impl Node {
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-enum Token<E>
+pub enum Token<E>
 where
     E: Copy + Eq + Hash + Debug,
 {
@@ -38,7 +38,7 @@ impl<E> Token<E>
 where
     E: Copy + Eq + Hash + Debug,
 {
-    fn is_elem(&self) -> bool {
+    pub fn is_elem(&self) -> bool {
         if let Self::Element(_) = self {
             true
         } else {
@@ -56,15 +56,64 @@ where
     nodes: Vec<Node>,
     edges: HashMap<usize, HashMap<Token<E>, usize>>,
     links: HashMap<usize, usize>,
+    empty: HashMap<Token<E>, usize>, // workaround for lack of global generic
 }
 
 pub struct NodeView<'a, E>
 where
     E: Copy + Eq + Hash + Debug,
 {
-    pub span: &'a [E],
-    pub edges: HashMap<E, usize>,
-    pub terminators: Vec<usize>,
+    st: &'a GeneralizedSuffixTree<E>,
+    id: usize,
+    edges: &'a HashMap<Token<E>, usize>,
+}
+
+impl<'a, E> NodeView<'a, E>
+where
+    E: Default + Copy + Eq + Hash + Debug,
+{
+    pub fn id(&self) -> usize {
+        self.id
+    }
+    pub fn source_id(&self) -> usize {
+        self.st.node_unsafe(self.id).source
+    }
+    pub fn source(&self) -> &'a [E] {
+        &self.st.elems[self.st.node_unsafe(self.id).source]
+    }
+    pub fn span_start(&self) -> usize {
+        self.st.node_unsafe(self.id).start.saturating_sub(1) // edge case for root (0, 0)
+    }
+    pub fn span_end(&self) -> usize {
+        let node = self.st.node_unsafe(self.id);
+        if node.end == usize::MAX {
+            self.st.elems[node.source].len()
+        } else {
+            node.end
+        }
+    }
+    pub fn span(&'a self) -> &'a [E] {
+        &self.source()[self.span_start()..self.span_end()]
+    }
+    pub fn len(&self) -> usize {
+        self.span_end() - self.span_start()
+    }
+    pub fn edges(&self) -> &HashMap<Token<E>, usize> {
+        &self.edges
+    }
+    pub fn link(&self) -> Option<NodeView<'a, E>> {
+        self.st
+            .links
+            .get(&self.id)
+            .map(|&id| self.st.node(id))
+            .flatten()
+    }
+    pub fn link_id(&self) -> Option<usize> {
+        self.st.links.get(&self.id).copied()
+    }
+    pub fn is_terminal(&self) -> bool {
+        self.edges.is_empty()
+    }
 }
 
 impl<E, I, const N: usize> From<[I; N]> for GeneralizedSuffixTree<E>
@@ -120,58 +169,20 @@ where
         ROOT
     }
 
-    pub fn node_count(&self) -> usize {
+    pub fn nodes_count(&self) -> usize {
         self.nodes.len()
     }
 
-    pub fn get_node(&self, id: usize) -> Option<NodeView<E>> {
+    pub fn node(&self, id: usize) -> Option<NodeView<E>> {
         if id < 1 || id > self.nodes.len() {
             return None; // boundary checks
         }
-        let node = self.node(id);
-        if node.start > self.elems[node.source].len() {
-            return None; // don't return terminator nodes
-        }
-        let span = &self.elems[node.source][node.start..=node.end.min(self.end(node.source) - 1)];
-        let edges = self
-            .edges
-            .get(&id)
-            .map(|edges| {
-                let mut elem_edges = HashMap::new();
-                for (&token, &child) in edges {
-                    match token {
-                        Token::Element(elem) => {
-                            elem_edges.insert(elem, child);
-                        }
-                        _ => (),
-                    }
-                }
-                elem_edges
-            })
-            .unwrap_or_else(HashMap::new);
-        let mut terminators = self
-            .edges
-            .get(&id)
-            .map(|edges| {
-                let mut terminators = Vec::new();
-                for (&token, _) in edges {
-                    match token {
-                        Token::Terminator(source) => terminators.push(source),
-                        _ => {}
-                    }
-                }
-                terminators
-            })
-            .unwrap_or_else(Vec::new);
-
-        if node.end >= self.elems[node.source].len() {
-            terminators.push(node.source); // leaf node terminators are implicit
-        }
+        let edges = self.edges.get(&id).unwrap_or(&self.empty);
 
         Some(NodeView {
-            span,
+            st: &self,
+            id,
             edges,
-            terminators,
         })
     }
 
@@ -198,14 +209,14 @@ where
         }
     }
 
-    fn edge(&self, node: usize, key: Token<E>) -> usize {
+    fn edge_unsafe(&self, node: usize, key: Token<E>) -> usize {
         match node {
             0 => 1,
             n => self.edges[&n][&key],
         }
     }
 
-    fn get_edge(&self, node: usize, elem: E) -> Option<usize> {
+    fn edge(&self, node: usize, elem: E) -> Option<usize> {
         match node {
             0 => Some(1),
             n => self
@@ -217,19 +228,19 @@ where
         }
     }
 
-    fn node(&self, node: usize) -> &Node {
+    fn node_unsafe(&self, node: usize) -> &Node {
         &self.nodes[node - 1]
     }
 
-    fn node_mut(&mut self, node: usize) -> &mut Node {
+    fn node_unsafe_mut(&mut self, node: usize) -> &mut Node {
         &mut self.nodes[node - 1]
     }
 
-    fn link(&self, node: usize) -> usize {
+    fn node_link_unsafe(&self, node: usize) -> usize {
         self.links[&node]
     }
 
-    fn get(&self, source: usize, pos: usize) -> Token<E> {
+    fn get_elem(&self, source: usize, pos: usize) -> Token<E> {
         if pos <= self.elems[source].len() {
             Token::Element(self.elems[source][pos - 1])
         } else {
@@ -237,15 +248,15 @@ where
         }
     }
 
-    fn get_elem(&self, source: usize, pos: usize) -> E {
+    fn get_elem_unsafe(&self, source: usize, pos: usize) -> E {
         self.elems[source][pos - 1] // panics if index is outside of range
     }
 
-    pub fn get_source(&self, source: usize) -> &[E] {
-        &self.elems[source]
+    pub fn get_source(&self, source: usize) -> Option<&[E]> {
+        self.elems.get(source).map(Vec::as_slice)
     }
 
-    fn end(&self, source: usize) -> usize {
+    fn node_end(&self, source: usize) -> usize {
         self.elems[source].len()
     }
 
@@ -283,7 +294,7 @@ where
         mut start: usize,
         i: usize,
     ) -> (usize, usize) {
-        let t_i = self.get(source, i);
+        let t_i = self.get_elem(source, i);
         let mut prev = ROOT;
         let (mut endpoint, mut curr) = self.test_and_split(source, node, start, i - 1, t_i);
         while !endpoint {
@@ -293,7 +304,7 @@ where
                 self.add_link(prev, curr);
             }
             prev = curr;
-            (node, start) = self.canonize(source, self.link(node), start, i - 1);
+            (node, start) = self.canonize(source, self.node_link_unsafe(node), start, i - 1);
             (endpoint, curr) = self.test_and_split(source, node, start, i - 1, t_i);
         }
         if prev != ROOT {
@@ -311,21 +322,21 @@ where
         t: Token<E>,
     ) -> (bool, usize) {
         if start <= end {
-            let t_start = self.get(source, start);
-            let child = self.edge(node, t_start);
-            let t_mid = self.get(
-                self.node(child).source,
-                1 + self.node(child).start + end - start,
+            let t_start = self.get_elem(source, start);
+            let child = self.edge_unsafe(node, t_start);
+            let t_mid = self.get_elem(
+                self.node_unsafe(child).source,
+                1 + self.node_unsafe(child).start + end - start,
             );
             if t == t_mid {
                 (true, node)
             } else {
                 let mid = self.new_node(
-                    self.node(child).source,
-                    self.node(child).start,
-                    self.node(child).start + end - start,
+                    self.node_unsafe(child).source,
+                    self.node_unsafe(child).start,
+                    self.node_unsafe(child).start + end - start,
                 );
-                self.node_mut(child).start += end - start + 1;
+                self.node_unsafe_mut(child).start += end - start + 1;
                 self.add_edge(mid, t_mid, child);
                 self.add_edge(node, t_start, mid);
                 (false, mid)
@@ -347,14 +358,16 @@ where
         end: usize,
     ) -> (usize, usize) {
         if end >= start {
-            let mut t_start = self.get(source, start);
-            let mut child = self.edge(node, t_start);
-            while end >= start && self.node(child).end - self.node(child).start <= end - start {
-                start += self.node(child).end - self.node(child).start + 1;
+            let mut t_start = self.get_elem(source, start);
+            let mut child = self.edge_unsafe(node, t_start);
+            while end >= start
+                && self.node_unsafe(child).end - self.node_unsafe(child).start <= end - start
+            {
+                start += self.node_unsafe(child).end - self.node_unsafe(child).start + 1;
                 node = child;
                 if start <= end {
-                    t_start = self.get(source, start);
-                    child = self.edge(node, t_start);
+                    t_start = self.get_elem(source, start);
+                    child = self.edge_unsafe(node, t_start);
                 }
             }
         }
@@ -369,22 +382,24 @@ where
         let n = pattern.len();
         let mut stack = Vec::from([(ROOT, 0)]);
         'outer: while let Some((node_id, i)) = stack.pop() {
-            if let Some(child_id) = self.get_edge(node_id, pattern[i]) {
-                let child = self.node(child_id);
-                let m: usize = child.len().min(self.end(child.source) - child.start + 1);
+            if let Some(child_id) = self.edge(node_id, pattern[i]) {
+                let child = self.node_unsafe(child_id);
+                let m: usize = child
+                    .len()
+                    .min(self.node_end(child.source) - child.start + 1);
                 for j in 1..m {
                     if i + j == n {
                         // found a match
                         return Some((child.source, child.start + j - n - 1));
                     }
 
-                    if self.get_elem(child.source, child.start + j) != pattern[i + j] {
+                    if self.get_elem_unsafe(child.source, child.start + j) != pattern[i + j] {
                         continue 'outer;
                     }
                 }
                 if i + m == n {
                     // found a match
-                    return Some((child.source, child.end.min(self.end(child.source)) - n));
+                    return Some((child.source, child.end.min(self.node_end(child.source)) - n));
                 } else {
                     stack.push((child_id, i + m));
                 }
@@ -398,9 +413,11 @@ where
         let mut matches = Vec::new();
         let mut stack = Vec::from([(ROOT, 0)]);
         'outer: while let Some((node_id, i)) = stack.pop() {
-            if let Some(child_id) = self.get_edge(node_id, pattern[i]) {
-                let child = self.node(child_id);
-                let m: usize = child.len().min(self.end(child.source) - child.start + 1);
+            if let Some(child_id) = self.edge(node_id, pattern[i]) {
+                let child = self.node_unsafe(child_id);
+                let m: usize = child
+                    .len()
+                    .min(self.node_end(child.source) - child.start + 1);
                 for j in 1..m {
                     if i + j == n {
                         // found a match
@@ -408,7 +425,7 @@ where
                         continue 'outer;
                     }
 
-                    if self.get_elem(child.source, child.start + j) != pattern[i + j] {
+                    if self.get_elem_unsafe(child.source, child.start + j) != pattern[i + j] {
                         continue 'outer;
                     }
                 }
@@ -427,8 +444,8 @@ where
         let mut pos = Vec::new();
         let mut stack = Vec::from([(node, depth)]);
         while let Some((node_id, depth)) = stack.pop() {
-            let node = self.node(node_id);
-            let m = node.len().min(self.end(node.source) - node.start + 1);
+            let node = self.node_unsafe(node_id);
+            let m = node.len().min(self.node_end(node.source) - node.start + 1);
             if let Some(edges) = self.edges.get(&node_id) {
                 for (&token, &child) in edges {
                     match token {
@@ -436,15 +453,75 @@ where
                             stack.push((child, depth + m));
                         }
                         Token::Terminator(source) => {
-                            pos.push((source, self.end(source) - depth - m));
+                            pos.push((source, self.node_end(source) - depth - m));
                         }
                     }
                 }
             } else {
-                pos.push((node.source, self.end(node.source) - depth - m));
+                pos.push((node.source, self.node_end(node.source) - depth - m));
             }
         }
         pos
+    }
+
+    fn find_lcp_recursive(
+        &self,
+        threshold: usize,
+        min_length: usize,
+        node_id: usize,
+        length: usize,
+        lcp: &mut BinaryHeap<(usize, usize)>,
+    ) -> Option<HashSet<usize>> {
+        let node = self.node(node_id).unwrap();
+        let mut sources = if node.is_terminal() {
+            HashSet::from([node.source_id()])
+        } else {
+            HashSet::new()
+        };
+        let mut propagate = false;
+        for &child_id in node.edges().values() {
+            if let Some(child_sources) =
+                self.find_lcp_recursive(threshold, min_length, child_id, length + node.len(), lcp)
+            {
+                sources.extend(child_sources);
+            } else {
+                propagate = true;
+            }
+        }
+        if propagate || length + node.len() < min_length {
+            None
+        } else if sources.len() >= threshold {
+            lcp.push((length + node.len(), node_id));
+            None
+        } else {
+            Some(sources)
+        }
+    }
+
+    fn find_lcp(&self, threshold: usize, min_length: usize) -> BinaryHeap<(usize, usize)> {
+        let mut lcp = BinaryHeap::new();
+        self.find_lcp_recursive(threshold, min_length, self.root(), 0, &mut lcp);
+        lcp
+    }
+
+    pub fn find_common(&self, threshold: usize, min_length: usize) -> Vec<&[E]> {
+        let mut common = Vec::new();
+        let mut to_visit = self.find_lcp(threshold, min_length);
+        let mut visited = HashSet::new();
+        while let Some((length, node_id)) = to_visit.pop() {
+            if visited.contains(&node_id) {
+                continue;
+            }
+
+            let mut node = self.node(node_id).unwrap();
+            common.push(&node.source()[node.span_end() - length..node.span_end()]);
+
+            while let Some(next) = node.link() {
+                visited.insert(next.id());
+                node = next;
+            }
+        }
+        common
     }
 }
 
@@ -464,18 +541,23 @@ where
             while let Some((curr, mut prefix, is_last)) = stack.pop() {
                 let to_add = if is_last { "└──" } else { "├──" };
                 let curr_prefix: String = prefix.chars().chain(to_add.chars()).collect();
-                let span = if self.node(curr).start > self.end(self.node(curr).source) {
+                let span = if self.node_unsafe(curr).start
+                    > self.node_end(self.node_unsafe(curr).source)
+                {
                     format!("T")
                 } else {
                     format!(
                         "{:?}",
-                        &self.elems[self.node(curr).source][self.node(curr).start - 1
-                            ..self.node(curr).end.min(self.end(self.node(curr).source))]
+                        &self.elems[self.node_unsafe(curr).source][self.node_unsafe(curr).start - 1
+                            ..self
+                                .node_unsafe(curr)
+                                .end
+                                .min(self.node_end(self.node_unsafe(curr).source))]
                     )
                 };
                 write!(fmt, "{curr_prefix} {curr} {span}",)?;
                 if !self.edges.contains_key(&curr) {
-                    write!(fmt, ":{}", self.node(curr).source)?;
+                    write!(fmt, ":{}", self.node_unsafe(curr).source)?;
                 }
                 if self.links.contains_key(&curr) {
                     write!(fmt, " ➔ {}", self.links[&curr])?;
@@ -501,6 +583,23 @@ where
 mod tests {
 
     use super::*;
+
+    // #[test]
+    // fn test_foobar() {
+    //     let gst = GeneralizedSuffixTree::from([
+    //         "foo-123-bar".to_owned().chars(),
+    //         "foo-456-bar".to_owned().chars(),
+    //         "xyz-bar".to_owned().chars(),
+    //         "foouvw".to_owned().chars(),
+    //         "foopqr".to_owned().chars(),
+    //         "foooqy".to_owned().chars(),
+    //     ]);
+    //     println!("{gst}");
+
+    //     for common in gst.find_common(2, 2) {
+    //         println!("{}", String::from_iter(common));
+    //     }
+    // }
 
     #[test]
     fn test_contains_abab() {
